34eeef80c15ba52833813d4e0d6bdab2d18e9484
Alex Berry <berry3@indiana.edu>
2015-06-14 21:02:19 -0400

    renaming src/site directory + adding copy resource task

--- buildSrc/src/main/groovy/SiteBuilderPlugin.groovy
+++ buildSrc/src/main/groovy/SiteBuilderPlugin.groovy
@@ -1,10 +1,22 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 
 class SiteBuilderPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.task('buildSite') << {
+
+        project.task('processResources', type:Copy) {
+            from("$project.projectDir/src/site/resources")
+            into "$project.buildDir/website"
+        }
+
+        project.task('buildSite', dependsOn:[':processResources']) << {
+            //TODO process md blog posts
+
+            //TODO fill into index
+
+            //TODO write index
 
         }
     }
