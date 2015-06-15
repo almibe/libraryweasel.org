c077cd6ae653820e4554c399790e2f11a3af6b12
Alex Berry <berry3@indiana.edu>
2015-06-14 22:28:40 -0400

    building up blog objects based on json, md, and html files

--- buildSrc/src/main/groovy/org/libraryweasel/website/SiteBuilderPlugin.groovy
+++ buildSrc/src/main/groovy/org/libraryweasel/website/SiteBuilderPlugin.groovy
@@ -1,12 +1,21 @@
 package org.libraryweasel.website
 
+import com.google.common.io.Files
+import groovy.io.FileType
+import groovy.json.JsonSlurper
+import groovy.transform.Immutable
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Copy
+import org.pegdown.PegDownProcessor
 
 class SiteBuilderPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
+
+        JsonSlurper jsonSlurper = new JsonSlurper()
+        FileExtensionMapper fileExtensionMapper = new FileExtensionMapper()
+        PegDownProcessor markDownProcessor = new PegDownProcessor()
 
         project.task('processResources', type:Copy) {
             from("$project.projectDir/src/site/resources")
@@ -15,11 +24,30 @@ class SiteBuilderPlugin implements Plugin<Project> {
 
         project.task('buildSite', dependsOn:[':processResources']) << {
             //TODO process md blog posts
-
+            File dir = new File(project.projectDir, "src/site/contents/blog")
+            def blogs = []
+            dir.eachFile(FileType.FILES) { file ->
+                if (Files.getFileExtension(file.getName()) == 'json') {
+                    def blogMetadata = jsonSlurper.parse(file)
+                    if(fileExtensionMapper.switchFileExtension(file, 'md').isFile()) {
+                        blogs << new Blog(blogMetadata.blogTitle, blogMetadata.pubDate, markDownProcessor.markdownToHtml(fileExtensionMapper.switchFileExtension(file, 'md').text))
+                    } else if (fileExtensionMapper.switchFileExtension(file, 'html').isFile()) {
+                        blogs << new Blog(blogMetadata.blogTitle, blogMetadata.pubDate, fileExtensionMapper.switchFileExtension(file, 'html').text)
+                    } else {
+                        throw new RuntimeException("$file.path does not have content file.")
+                    }
+                }
+            }
             //TODO fill into index
-
             //TODO write index
-
+            //process index template and write index
         }
     }
+}
+
+@Immutable
+class Blog {
+    String title
+    String date
+    String content
 }
